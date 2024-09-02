d7941ec424b1d35c6cf6edcd6930cc63c83a8d43
duytung95nb <duytung95nb@gmail.com>
2024-09-02 22:11:34 +0700

    Add cors examples for cookie-based and http authentication

--- app/cors/public/index.js
+++ app/cors/public/index.js
@@ -1,1 +1,109 @@
+async function getRequest() {
+  try {
+    const response = await fetch("http://localhost:3000");
+    console.log("Response: ", await response.text());
+  } catch (e) {
+    console.error("Error requesting", e.message);
+  }
+}
+async function postRequest() {
+  try {
+    const response = await fetch("http://localhost:3000", {
+      method: "post",
+    });
+    console.log("Response: ", await response.text());
+  } catch (e) {
+    console.error("Error requesting", e.message);
+  }
+}
 
+async function authenticateWithCookie() {
+  try {
+    const response = await fetch("http://localhost:3000/authenticate/cookie", {
+      method: "post",
+      credentials: "include",
+    });
+    console.log("Response: ", await response.json());
+  } catch (e) {
+    console.error("Error requesting", e.message);
+  }
+}
+
+async function requestWithCookieSuccess() {
+  try {
+    const response = await fetch(
+      "http://localhost:3000/data/cookie-based/success",
+      {
+        method: "post",
+        credentials: "include",
+      }
+    );
+    console.log("Response: ", await response.json());
+  } catch (e) {
+    console.error("Error requesting", e.message);
+  }
+}
+
+async function requestWithCookieFail() {
+  try {
+    const response = await fetch(
+      "http://localhost:3000/data/cookie-based/fail",
+      {
+        method: "post",
+        credentials: "include",
+      }
+    );
+    console.log("Response: ", await response.json());
+  } catch (e) {
+    console.error("Error requesting", e.message);
+  }
+}
+async function authenticateWithHttp() {
+  try {
+    const response = await fetch(
+      "http://localhost:3000/authenticate/http-auth",
+      {
+        method: "post",
+        credentials: "include",
+      }
+    );
+    console.log("Response: ", await response.json());
+  } catch (e) {
+    console.error("Error requesting", e.message);
+  }
+}
+
+async function requestWithHttpAuthSuccess() {
+  try {
+    const response = await fetch(
+      "http://localhost:3000/data/http-auth/success",
+      {
+        method: "post",
+        credentials: "include",
+      }
+    );
+    console.log("Response: ", await response.json());
+  } catch (e) {
+    console.error("Error requesting", e.message);
+  }
+}
+
+async function requestWithHttpAuthFail() {
+  try {
+    const response = await fetch("http://localhost:3000/data/http-auth/fail", {
+      method: "post",
+      credentials: "include",
+    });
+    console.log("Response: ", response.json());
+  } catch (e) {
+    console.error("Error requesting", e.message);
+  }
+}
+
+async function main() {
+  const responses = await Promise.allSettled([getRequest(), postRequest()]);
+  console.log("All responses", responses);
+}
+(async () => {
+  await main();
+})();
--- app/server/src/index.js
+++ app/server/src/index.js
@@ -1,9 +1,139 @@
 const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+app.use(cookieParser());
+// Expect to allow all requests without credentials
+app.get("/", cors(), (req, res) => {
   res.send("Hello World!");
+});
+
+// Expect to allow all requests without credentials
+app.post("/", cors(), (req, res) => {
+  res.send("Hello World!");
+});
+
+// Need to specify explicit origin for credentialed request to enable cross-origin request
+const corsForCredentialedCookieRequest = cors({
+  credentials: true,
+  origin: "http://localhost:8080",
+  methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+});
+
+// Authenticate with cookie
+app.post(
+  "/authenticate/cookie",
+  corsForCredentialedCookieRequest,
+  (req, res) => {
+    if (req.cookies.user === "admin") {
+      res.status(200).send({
+        alreadyAuthenticatedBefore: true,
+      });
+      return;
+    }
+    res.cookie("user", "admin");
+    res.status(200).send({
+      authenticated: true,
+    });
+  }
+);
+
+// Request resource success in cookie-based auth
+app.post(
+  "/data/cookie-based/success",
+  corsForCredentialedCookieRequest,
+  (req, res) => {
+    if (req.cookies.user === "admin") {
+      res.status(200).send({
+        resourceDataReturned: true,
+      });
+    } else {
+      res.status(401).send({
+        message: "You are not authenticated!",
+      });
+    }
+  }
+);
+
+// Request resource failed in cookie-based auth (due to cors allow '*')
+app.post("/data/cookie-based/fail", cors(), (req, res) => {
+  console.log(
+    "CORS request will still reach here but browser will throw net::ERR_FAILED"
+  );
+  res.status(200).send({
+    succes: true,
+  });
+});
+
+// Need to specify explicit origin for credentialed request to enable cross-origin request
+const corsForCredentialedHttpRequest = cors({
+  credentials: true,
+  origin: "http://localhost:8080",
+  methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+});
+
+// Basic authentication
+app.post(
+  "/authenticate/http-auth",
+  corsForCredentialedHttpRequest,
+  (req, res) => {
+    const b64auth = (req.headers.authorization || "").split(" ")[1] || "";
+    // Username and password in form of: <username>:<password>
+    const [username, password] = Buffer.from(b64auth, "base64")
+      .toString()
+      .split(":");
+    if (username === "admin" && password === "admin") {
+      res.status(200).send({
+        alreadyAuthenticatedBefore: true,
+      });
+      return;
+    }
+    res
+      .status(401)
+      .setHeader("WWW-Authenticate", 'Basic reaml="myServer"')
+      .send({
+        authenticated: true,
+      });
+  }
+);
+function httpAuthMiddleware(req, res, next) {
+  const b64auth = (req.headers.authorization || "").split(" ")[1] || "";
+  // Username and password in form of: <username>:<password>
+  const [username, password] = Buffer.from(b64auth, "base64")
+    .toString()
+    .split(":");
+  if (username === "admin" && password === "admin") {
+    req.userData = { username, password };
+    next();
+    return;
+  }
+  res.status(401).setHeader("WWW-Authenticate", 'Basic reaml="myServer"').send({
+    authenticated: true,
+  });
+}
+// Request resource success in http auth
+app.post(
+  "/data/http-auth/success",
+  corsForCredentialedHttpRequest,
+  httpAuthMiddleware,
+  (req, res) => {
+    res.status(200).send({
+      username: req.userData.username,
+      success: true,
+    });
+  }
+);
+
+// Request resource fail in http auth (due to cors allow '*')
+app.post("/data/http-auth/fail", cors(), (req, res) => {
+  console.log(
+    "CORS request will still reach here but browser will throw net::ERR_FAILED"
+  );
+  res.status(200).send({
+    succes: true,
+  });
 });
 
 app.listen(port, () => {
